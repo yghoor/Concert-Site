0087d847691e03ae01ad44fbfd3f78146b3659d5
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-02 00:15:38 +0200

    Create array of objects with each speakers' info

--- script-index.js
+++ script-index.js
@@ -11,3 +11,42 @@ function changeIcon() {
     openIcon.classList.remove('d-none');
   }
 }
+
+const speakers = [
+  {
+    name: 'Dorothy Smith',
+    image: 'images/speakers/dorothy-smith.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    name: 'Fonje Distry',
+    image: 'images/speakers/fonje-distry.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    name: 'Ingrid Son',
+    image: 'images/speakers/ingrid-son.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    name: 'James Dodes',
+    image: 'images/speakers/james-dodes.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    name: 'Jim Genes',
+    image: 'images/speakers/jim-genes.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    name: 'Rodríguez Jóhannsson',
+    image: 'images/speakers/rodríguez-jóhannsson.jpg',
+    workInfo: 'An example of the speaker\'s current job and position',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.'
+  },
+];
